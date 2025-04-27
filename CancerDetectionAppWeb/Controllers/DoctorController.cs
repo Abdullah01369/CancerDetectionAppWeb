63058cc5d12ea9dff144ed6be926f1c876fefeec
Abdullah ARICI <icerde186@gmail.com>
2025-04-27 07:31:51 +0300

    Add project files.

---
+++ CancerDetectionAppWeb/Controllers/DoctorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CancerDetectionAppWeb.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult PatientsPage()
+        {
+            return View();
+        }
+
+        public IActionResult PatientResultDetail()
+        {
+            return View();
+        }
+    }
+}
